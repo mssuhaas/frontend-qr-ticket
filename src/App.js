892382e6dd52c0dba40c7eab146ded5880b18830
Surya Suhaas <mssuhaas@gmail.com>
2024-05-05 12:12:03 +0530

    Made a menu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import TicketForm from './TicketForm';
+import { Container, Typography, CssBaseline, AppBar, Toolbar, IconButton, Switch } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Brightness4 as DarkModeIcon, Brightness7 as LightModeIcon } from '@mui/icons-material';
+
+// Custom dark mode theme
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+// Custom light mode theme
+const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+});
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  const currentTheme = darkMode ? darkTheme : lightTheme;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={currentTheme}>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'center' }}>
+         Book Tickets
+        </Typography>
+
+          <IconButton onClick={toggleDarkMode} color="inherit">
+            {darkMode ? <LightModeIcon /> : <DarkModeIcon />}
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="sm" style={{ marginTop: '20px' }}>
+        <TicketForm />
+      </Container>
+    </ThemeProvider>
   );
 }
 
